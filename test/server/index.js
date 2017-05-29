818eaf1fae1b98496e1a72626b6aa7207a6d4265
Todd Wolfson <todd@twolfson.com>
2017-05-29 02:02:48 -0700

    Completed our initial tests

--- test/server/index.js
+++ test/server/index.js
@@ -1,8 +1,19 @@
 // Load in dependencies
+var expect = require('chai').expect;
+var httpUtils = require('./utils/http');
+var serverUtils = require('./utils/server');
 
 // Start our tests
 describe('A request to our server', function () {
+  httpUtils.save({
+    url: serverUtils.getUrl('/'),
+    expectedStatusCode: 200
+  });
+
   it('has no errors', function () {
      // Asserted by `expectedStatusCode`
+  });
+  it('has expected content', function () {
+    expect(this.body).to.equal('OK');
   });
 });
