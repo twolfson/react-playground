675d0dd80fa3268110ab830ef3789f9dcc6c53ef
Todd Wolfson <todd@twolfson.com>
2017-05-29 23:54:17 -0700

    Enabled skipped test

--- test/server/root-show.js
+++ test/server/root-show.js
@@ -6,7 +6,7 @@ import * as serverUtils from './utils/server';
 
 // Start our tests
 describe('A request to GET /', function () {
-  describe.skip('from an authenticated user', function () {
+  describe('from an authenticated user', function () {
     httpUtils.session.init().login()
       .save({
         url: serverUtils.getUrl('/'),
@@ -14,7 +14,7 @@ describe('A request to GET /', function () {
       });
 
     it('shows user as logged in', function () {
-      expect(this.body).to.contain('You are logged in as:');
+      expect(this.body).to.contain('You are logged in as: dev-user');
     });
     it('has a login form', function () {
       expect(this.$('form[action="/login"]')).to.have.lengthOf(1);
