86594b5b9a28970f9c1b8ac3f4d39eca7039b763
Todd Wolfson <todd@twolfson.com>
2017-06-04 17:05:12 -0700

    Attempting to iterate on mutations further

--- server/models/root.js
+++ server/models/root.js
@@ -2,7 +2,7 @@
 import httpErrors from 'http-errors';
 import {GraphQLObjectType, GraphQLString} from 'graphql';
 
-// Define our root object type and mutation object type
+// Define our root object type
 // TODO: Add description to object type and fields
 export const RootQueryObjectType = new GraphQLObjectType({
   name: 'RootQueryObjectType',
@@ -41,8 +41,18 @@ export const RootQueryObjectType = new GraphQLObjectType({
   }
 });
 
-const PostOutputType = new GraphQLObjectType({
-  name: 'PostOutputType',
+// Define our root mutation object type
+// Mutations are based on
+//   https://github.com/graphql/graphql-relay-js/blob/v0.5.2/src/mutation/mutation.js#L59-L112
+// TODO: What's the difference between `InputObjectType` and `ObjectType`?
+const createPostInputType = new GraphQLInputObjectType({
+  name: 'createPostInputType',
+  fields: {
+    content: {type: GraphQLString}
+  }
+});
+const createPostOutputType = new GraphQLObjectType({
+  name: 'createPostOutputType',
   fields: {
     content: {type: GraphQLString}
   }
@@ -52,12 +62,12 @@ export const RootMutationObjectType = new GraphQLObjectType({
   name: 'RootMutationObjectType',
   fields: {
     createPost: {
-      type: PostOutputType,
+      type: createPostOutputType,
       args: {
-        content: {type: GraphQLString}
+        input: {type: createPostInputType}
       },
       resolve(value, args, request) {
-        return new PostOutputType(args.content);
+        return new createPostOutputType(args.content);
       }
     }
   }
