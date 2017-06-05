a5363f0c4e8379c464147001517e82cf655447c7
Todd Wolfson <todd@twolfson.com>
2017-06-04 17:34:35 -0700

    Attempting to get variables working with GraphQL

--- test/server/graphql/query-echo.js
+++ test/server/graphql/query-echo.js
@@ -4,7 +4,7 @@ import {expect} from 'chai';
 import * as httpUtils from '../utils/http';
 
 // Define our tests
-describe('A GraphQL query request for `echo`', function () {
+describe('A GraphQL query request for `echo` via arguments', function () {
   httpUtils.graphql({
     body: `
       query {
@@ -15,6 +15,27 @@ describe('A GraphQL query request for `echo`', function () {
   });
 
   it('reuses input as output', function () {
+    expect(this.json).to.deep.equal({data: {echo: 'hi'}});
+  });
+});
+
+describe.only('A GraphQL query request for `echo` via variables', function () {
+  httpUtils.graphql({
+    body: JSON.stringify({
+      query: `
+        query ($content: String) {
+          echo(content: $content)
+        }
+      `,
+      variables: {
+        content: 'hi'
+      }
+    }),
+    expectedStatusCode: 200
+  });
+
+  it('reuses input as output', function () {
+    console.log('wat', this.json);
     expect(this.json).to.deep.equal({data: {echo: 'hi'}});
   });
 });
