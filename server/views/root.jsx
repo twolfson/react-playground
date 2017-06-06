a9f131cd1df079786d7af88b2169de258928bd92
Todd Wolfson <todd@twolfson.com>
2017-06-05 23:23:25 -0700

    Removed TODO for propTypes, looks like it's not possible

--- server/views/root.jsx
+++ server/views/root.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 
 // Export our view
 export default class RootView extends React.Component {
-  // TODO: Get propTypes to throw error on mismatch instead of `console.warn`
   static propTypes = {
     email: PropTypes.string
   };
