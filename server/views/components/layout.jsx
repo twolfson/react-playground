db6e06d9fe4d544da447fb60cb125a66bdca58f8
Todd Wolfson <todd@twolfson.com>
2017-06-06 00:03:45 -0700

    Added reference to Layout

--- server/views/components/layout.jsx
+++ server/views/components/layout.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Export our view
+// Based on https://github.com/reactjs/express-react-views/tree/v0.10.1
 export default class Layout extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired
