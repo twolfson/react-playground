a04ed5611f8111effec80cf168b45d6814f3b90e
Todd Wolfson <todd@twolfson.com>
2017-06-06 02:00:39 -0700

    Added smart exclude for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /(\.js|\.jsx)$/, use: jsLoaders = ['babel-loader']}
+      {test: /(\.js|\.jsx)$/, use: jsLoaders = ['babel-loader'], exclude: /node_modules/}
     ]
   },
   plugins: []
