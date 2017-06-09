392bcf5f0290f31d38ef713ff51490b0fa48ab48
Todd Wolfson <todd@twolfson.com>
2017-06-08 21:19:37 -0700

    Got something working in our repo

--- browser/js/posts.jsx
+++ browser/js/posts.jsx
@@ -1,33 +1,16 @@
-// Load in our dependencies
+import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {AppContainer} from 'react-hot-loader';
-
 import Counter from './counter';
 
-// When our page loads
-// window.addEventListener('DOMContentLoaded', function handleReady () {
-  // Find our target element
-  let targetEl = document.querySelector('#main');
-  if (!targetEl) { throw new Error('Unable to find our target element'); }
+const rootEl = document.getElementById('main');
+const render = Component =>
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    rootEl
+  );
 
-  // Output content with HMR wrapper
-  const render = (Component) => {
-    console.log('re-render!');
-    ReactDOM.render(
-      <AppContainer>
-        <Component />
-      </AppContainer>,
-      targetEl);
-  };
-  render(Counter);
-
-  // If we have HMR enabled, handle reloads
-  // TODO: Tidy up errors
-  if (module.hot) {
-    module.hot.accept('./counter', () => {
-      console.log('reeeerender');
-      render(x);
-    });
-  }
-// });
+render(Counter);
+if (module.hot) module.hot.accept('./counter', () => render(Counter));
