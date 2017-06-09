0a0daf18c7e7cae5798d02e354d7035491669816
Todd Wolfson <todd@twolfson.com>
2017-06-09 00:26:35 -0700

    Started on data fetching

--- browser/js/posts-app.jsx
+++ browser/js/posts-app.jsx
@@ -3,6 +3,24 @@ import React from 'react';
 
 // Define our application component
 export default class PostsApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {posts: [], loading: true};
+    this._fetchData();
+  }
+
+  _fetchData() {
+    // Fetch our posts from GraphQL
+    // TODO: Add tests for me
+    // DEV: We will worry about hydration via props in the next step
+    //   Maybe define a singleton state and run `init` on that
+    console.log('hiiii');
+  }
+
+  componentWillUpdate() {
+    window.reload();
+  }
+
   render() {
     return (
       <div>
