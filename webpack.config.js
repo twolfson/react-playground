8db9370152cf35d6b308dcc00629de0ffb6cda3c
Todd Wolfson <todd@twolfson.com>
2017-06-08 21:51:36 -0700

    Relocated HMR bindings to HMR setup in webpack.config.js only

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@
 /* eslint-disable no-restricted-globals,global-require */
 
 // Load in our dependencies
+const assert = require('assert');
+const fs = require('fs');
 const url = require('url');
 
 const _ = require('underscore');
@@ -11,6 +13,7 @@ const {getConfig} = require('./config');
 
 // Define our Webpack config
 const config = getConfig();
+let babelConfig = JSON.parse(fs.readFileSync(__dirname + '/.babelrc', 'utf8'));
 module.exports = {
   entry: ['./browser/js/posts-container.jsx'],
   output: {
@@ -28,7 +31,15 @@ module.exports = {
   },
   module: {
     rules: [
-      {test: /(\.js|\.jsx)$/, use: ['babel-loader'], exclude: /node_modules/}
+      {
+        test: /(\.js|\.jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          // DEV: We use `babelConfig` so it can be extended for HMR
+          options: babelConfig
+        }]
+      }
     ]
   },
   plugins: []
@@ -37,6 +48,8 @@ module.exports = {
 // If we are in development, then enable hot module replacement (HMR)
 // DEV: We found this worked via trial/error/snooping in `react-hot-boilerplate#next`
 //   https://github.com/gaearon/react-hot-boilerplate/blob/530fb8fee703e52eeeed894301af3d46d266cae9/webpack.config.js
+//   If something breaks, we recommend ditching the server and switching to `serve .` temporarily
+//   Then compare between the boilerplate and the code -- it should be an `index.html`, `index.js`, and `app.js`
 if (process.env.ENV === 'development') {
   module.exports.entry.unshift('react-hot-loader/patch');
   // http://localhost:5000/browser-dist/js/ -> http://localhost:35730/browser-dist/js/
@@ -45,6 +58,15 @@ if (process.env.ENV === 'development') {
   module.exports.plugins.push(new webpack.HotModuleReplacementPlugin());
   module.exports.plugins.push(new webpack.NamedModulesPlugin());
   module.exports.plugins.push(new webpack.NoEmitOnErrorsPlugin());
+  assert(babelConfig.presets.includes('env'));
+  babelConfig.presets = babelConfig.presets.map(function removeModulesFromEnv (preset) {
+    if (preset === 'env') {
+      return ['env', {modules: false}];
+    } else {
+      return preset;
+    }
+  });
+  babelConfig.plugins.unshift('react-hot-loader/babel');
   module.exports.devServer = {
     host: config.webpackDevServerUrl.hostname,
     port: config.webpackDevServerUrl.port,
