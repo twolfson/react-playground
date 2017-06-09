c1918e6231b80bf3f29ddc887be49638836fdefd
Todd Wolfson <todd@twolfson.com>
2017-06-08 21:26:34 -0700

    Moved back to our format -- we were calling a bad variable for re-render

--- browser/js/posts.jsx
+++ browser/js/posts.jsx
@@ -1,23 +1,31 @@
-import assert from 'assert';
+// Load in our dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
 
 import Counter from './counter';
 
-const targetEl = document.querySelector('#main');
-assert(targetEl, 'Unable to find our target element');
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    targetEl);
-};
+// When our page loads
+window.addEventListener('DOMContentLoaded', function handleReady () {
+  // Find our target element
+  let targetEl = document.querySelector('#main');
+  if (!targetEl) { throw new Error('Unable to find our target element'); }
 
-render(Counter);
-if (module.hot) {
-  module.hot.accept('./counter', () => {
-    render(Counter);
-  });
-}
+  // Output content with HMR wrapper
+  const render = (Component) => {
+    ReactDOM.render(
+      <AppContainer>
+        <Component />
+      </AppContainer>,
+      targetEl);
+  };
+  render(Counter);
+
+  // If we have HMR enabled, handle reloads
+  // TODO: Tidy up errors
+  if (module.hot) {
+    module.hot.accept('./counter', () => {
+      render(Counter);
+    });
+  }
+});
