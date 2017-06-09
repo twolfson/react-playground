e6223e7edadf247351c16dabe624dc6eba3c4bfc
Todd Wolfson <todd@twolfson.com>
2017-06-08 21:23:13 -0700

    Refactored and can't find the difference

--- browser/js/posts.jsx
+++ browser/js/posts.jsx
@@ -1,16 +1,23 @@
-import { AppContainer } from 'react-hot-loader';
+import assert from 'assert';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader';
+
 import Counter from './counter';
 
-const rootEl = document.getElementById('main');
-const render = Component =>
+const targetEl = document.querySelector('#main');
+assert(targetEl, 'Unable to find our target element');
+const render = (Component) => {
   ReactDOM.render(
     <AppContainer>
       <Component />
     </AppContainer>,
-    rootEl
-  );
+    targetEl);
+};
 
 render(Counter);
-if (module.hot) module.hot.accept('./counter', () => render(Counter));
+if (module.hot) {
+  module.hot.accept('./counter', () => {
+    render(Counter);
+  });
+}
