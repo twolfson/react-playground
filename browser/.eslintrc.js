2f56ad6e9c71d5828ab6fb5cfd4415f4388ae9df
Todd Wolfson <todd@twolfson.com>
2017-06-09 01:14:10 -0700

    Fixed JSX lint error

---
+++ browser/.eslintrc.js
@@ -0,0 +1,11 @@
+// Define our constants
+const OFF = 'off';
+const WARN = 'warn';
+
+// Define our config
+module.exports = {
+  rules: {
+    // Allow nested ternaries for JSX
+    'no-nested-ternary': OFF
+  }
+};
