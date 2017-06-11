6f46763c7e58a4af68c6a608e983d81bf05f51d4
Todd Wolfson <todd@twolfson.com>
2017-06-11 16:49:41 -0700

    Added browser test

---
+++ test/browser/posts-app.jsx
@@ -0,0 +1,11 @@
+// Load in our dependencies
+// const {expect} = require('chai');
+
+// Define our tests
+describe('A PostApp component', function () {
+  describe('with no content', function () {
+    it('renders empty content', function () {
+      if (2 !== 2) { throw new Error('hi'); }
+    });
+  });
+});
