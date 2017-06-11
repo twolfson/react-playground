eda614f2f993cb5b515e7906c7f4609af60e5a94
Todd Wolfson <todd@twolfson.com>
2017-06-10 23:14:06 -0700

    Started transition to Gulp for ideal LiveReload support

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+// Load in our dependencies
+const _ = require('underscore');
+const gulp = require('gulp');
+const gulpNotify = require('gulp-notify');
+const gulpLivereload = require('gulp-livereload');
+const rimraf = require('rimraf');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+
+const webpackConfig = require('./webpack.config.js');
+
+// Set up our configuration
+let config = {
+  allowFailures: false,
+  watchFiles: false
+};
+if (process.env.ENV === 'development') {
+  config.allowFailures = true;
+  config.watchFiles = true;
+}
+
+// Define our build tasks
+gulp.task('build-clean', function clean (done) {
+  // Remove all compiled files in `browser-dist/`
+  rimraf(__dirname + '/browser-dist/', done);
+});
+
+gulp.task('build-css', function buildJs () {
+  return gulp.src(['/browser/css/*.css'])
+    .pipe(gulp.dest('browser-dist/css'))
+    .pipe(gulpLivereload());
+});
+
+gulp.task('build-js', function buildJs () {
+  // Bundle Webpack content
+  let jsStream = gulp.src('browser/js/posts-container.jsx')
+    .pipe(webpackStream(_.defaults({
+      watch: config.watchFiles
+    }, webpackConfig), webpack));
+
+  // If we are allowing failures, then log them
+  if (config.allowFailures) {
+    jsStream.on('error', gulpNotify.onError());
+  }
+
+  // Return our stream
+  return jsStream
+    .pipe(gulp.dest('browser-dist/js'))
+    .pipe(gulpLivereload());
+});
+
+gulp.task('build', ['build-css', 'build-js']);
+
+// Define our development tasks
+gulp.task('livereload-update', function livereloadUpdate () {
+  gulpLivereload.reload();
+});
+
+// DEV: `['build']` requires that our build task runs once
+gulp.task('develop', ['build'], function develop () {
+  // Start a livereload server
+  gulpLivereload.listen();
+
+  // When one of our src files changes, re-run its corresponding task
+  gulp.watch(['server/**/*'], ['livereload-update']);
+  gulp.watch(['browser/css/**/*'], ['build-css']);
+});
