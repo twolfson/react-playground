272053fc0603076e02e2e932f87a833da31fc62f
Todd Wolfson <todd@twolfson.com>
2017-06-11 17:49:29 -0700

    Relocated browser test utils

--- test/browser/posts-app.jsx
+++ test/browser/posts-app.jsx
@@ -1,57 +1,13 @@
 // Load in our dependencies
 const {expect} = require('chai');
-const {mount} = require('enzyme');
 const React = require('react');
 
-// Define our test helpers
-let loggedDebugNotice = false;
-const testUtils = {
-  init(renderFn) {
-    let mountEl;
-    before(function runRenderFn () {
-      // Create a fixture entry point
-      mountEl = document.createElement('div');
-      document.body.appendChild(mountEl);
-
-      // Render our React component and bind it to the page
-      this.$el = mount(renderFn(), {
-        attachTo: mountEl
-      });
-    });
-    after(function cleanup () {
-      // Taken from https://github.com/twolfson/multi-image-mergetool/blob/1.32.1/test/browser/utils/application.js#L128-L147
-      // If we are on the debug page, expose everything
-      if (window.location.pathname === '/debug.html')  {
-        // If only 1 test is running, expose everything and stop
-        if (window.mocha.options.hasOnly) {
-          // eslint-disable-next-line no-console
-          console.info('/debug.html and `hasOnly` detected, ' +
-            'exposing `window.$el`');
-          window.$el = this.$el;
-          return;
-        }
-
-        // Notify user about debugging
-        if (!loggedDebugNotice) {
-          // eslint-disable-next-line no-console
-          console.info('/debug.html detected but no `.only`. ' +
-            'To visually debug tests/stop cleanup, add a `.only` to a test suite');
-          loggedDebugNotice = true;
-        }
-      }
-
-      // Perform our cleanup
-      this.$el.detach();
-      delete this.$el;
-      document.body.removeChild(mountEl);
-    });
-  }
-};
+const reactUtils = require('./utils/react');
 
 // Define our tests
 describe('A PostApp component', function () {
   describe('with no content', function () {
-    testUtils.init(function () {
+    reactUtils.mount(function () {
       return (<h1>Hi</h1>);
     });
 
