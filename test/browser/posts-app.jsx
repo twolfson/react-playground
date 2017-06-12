2d2644c8f15b5e9d2dacf032ced706259048d72a
Todd Wolfson <todd@twolfson.com>
2017-06-11 17:33:19 -0700

    Added Enzyme to tests

--- test/browser/posts-app.jsx
+++ test/browser/posts-app.jsx
@@ -1,11 +1,14 @@
 // Load in our dependencies
 const {expect} = require('chai');
+const {mount} = require('enzyme');
+const React = require('react');
 
 // Define our tests
 describe('A PostApp component', function () {
   describe('with no content', function () {
     it('renders empty content', function () {
-      expect(1).to.equal(1);
+      const wrapper = mount(<h1>Hi</h1>);
+      expect(wrapper.text()).to.equal('Hi');
     });
   });
 });
