f8f54664f3388e5f7564bc56121b54a000b5a4f3
Todd Wolfson <todd@twolfson.com>
2017-06-11 23:58:26 -0700

    Iterating on query matching for GraphQL

--- test/browser/utils/xhr.js
+++ test/browser/utils/xhr.js
@@ -1,6 +1,7 @@
 // Load in our dependencies
 const assert = require('assert');
 
+const _ = require('underscore');
 const sinon = require('sinon');
 
 // Define `xhrUtils.mock`
@@ -56,11 +57,16 @@ exports.GRAPHQL_LOADING_ONLY = {
   }
 };
 /* eslint-disable global-require */
+function loadGraphqlContract(graphqlContract) {
+  // Parse our query lines into a query
+  graphqlContract.request.query = graphqlContract.request.queryLines.join('\n');
+  return graphqlContract;
+}
 const graphqlContractsByFilepath = {
-  'posts-and-comments-empty-200.json': require(__dirname + '/../../test-files/graphql-contracts' +
-    '/posts-and-comments-empty-200.json'),
-  'posts-and-comments-full-200.json': require(__dirname + '/../../test-files/graphql-contracts' +
-    '/posts-and-comments-full-200.json')
+  'posts-and-comments-empty-200.json': loadGraphqlContract(require(__dirname + '/../../test-files/graphql-contracts' +
+    '/posts-and-comments-empty-200.json')),
+  'posts-and-comments-full-200.json': loadGraphqlContract(require(__dirname + '/../../test-files/graphql-contracts' +
+    '/posts-and-comments-full-200.json'))
 };
 /* eslint-enable global-require */
 exports.graphql = function (filepaths) {
@@ -74,15 +80,26 @@ exports.graphql = function (filepaths) {
   });
 
   // Return a mock XHR response for our GraphQL endpoint
-  // TODO: Find matching contracts
-  assert.strictEqual(graphqlContracts.length, 1,
-    'Only 1 GraphQL contract supported at a time due to lack of matching support');
   return {
     method: 'POST',
     url: '/graphql',
     fn: function (req) {
+      // Load our request body
+      const reqBody = JSON.parse(req.requestBody);
+      const reqQuery = reqBody.query;
+      assert(reqQuery, `No \`query\` was specified for "${req.requestBody}"`);
+      const variables = reqBody.variables || {};
+
+      // Find the first matching contract
+      const graphqlContract = _.find(graphqlContracts, function isMatchingContract (graphqlContract) {
+        // TODO: Perform parsing and subsetting
+        // TODO: Add variables matching
+        return graphqlContract.request.query === reqQuery;
+      });
+      assert(graphqlContract, `Unable to find matching GraphQL contract for query "${reqQuery}" ' +
+        'and variables "${JSON.stringify(variables)}"`);
+
       // http://sinonjs.org/releases/v2.3.4/fake-xhr-and-server/#simulating-server-responses
-      const graphqlContract = graphqlContracts[0];
       req.respond(graphqlContract.response.statusCode, {},
         JSON.stringify(graphqlContract.response.body));
     }
