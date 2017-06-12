3305c71f43528c6c2b9afbdd5d27e5323398be32
Todd Wolfson <todd@twolfson.com>
2017-06-11 17:38:58 -0700

    Adding debug functionality to tests

--- test/browser/posts-app.jsx
+++ test/browser/posts-app.jsx
@@ -3,12 +3,37 @@ const {expect} = require('chai');
 const {mount} = require('enzyme');
 const React = require('react');
 
+// Define our test helpers
+const testUtils = {
+  init(renderFn) {
+    let mountEl;
+    before(function runRenderFn () {
+      // Create a fixture entry point
+      mountEl = document.createElement('div');
+      document.body.appendChild(mountEl);
+
+      // Render our React component and bind it to the page
+      this.$el = mount(renderFn(), {
+        attachTo: mountEl
+      });
+    });
+    after(function cleanup () {
+      this.$el.detach();
+      delete this.$el;
+      document.body.removeChild(mountEl);
+    });
+  }
+};
+
 // Define our tests
 describe('A PostApp component', function () {
   describe('with no content', function () {
+    testUtils.init(function () {
+      return (<h1>Hi</h1>);
+    });
+
     it('renders empty content', function () {
-      const wrapper = mount(<h1>Hi</h1>);
-      expect(wrapper.text()).to.equal('Hi');
+      expect(this.$el.text()).to.equal('Hi');
     });
   });
 });
