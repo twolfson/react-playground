0ce0078b5816db000bc10fb724317de451a21ede
Todd Wolfson <todd@twolfson.com>
2017-06-15 22:22:27 -0700

    Completed our server render test

--- test/server/posts-show.js
+++ test/server/posts-show.js
@@ -1,5 +1,6 @@
 // Load in dependencies
 const assert = require('assert');
+const fs = require('fs');
 
 const {expect} = require('chai');
 
@@ -11,29 +12,28 @@ const PostsApp = require('../../browser/js/posts-app');
 
 // Define our test helper
 const renderUtils = {
-  swapProps: function (Component) {
-    before(function swapPropsFn () {
+  spyProps: function (Component) {
+    before(function spyPropsFn () {
       // Define our location to save props
-      assert.strictEqual(this.propsSetCount, undefined,
-        'Expected `renderUtils.swapProps` to not be called multiple times but it was');
-      this.propsSetCount = 0;
+      assert.strictEqual(this.propsArgs, undefined,
+        'Expected `renderUtils.spyProps` to not be called multiple times but it was');
+      this.propsArgs = [];
 
       // Define our props get/set hooks
       const that = this;
       Object.defineProperty(Component.prototype, 'props', {
+        configurable: true,
         get: function () {
-          return that.props;
+          return that.propsArgs[that.propsArgs.length - 1];
         },
         set: function (_props) {
-          that.propsSetCount += 1;
-          that.props = _props;
+          that.propsArgs.push(_props);
         }
       });
     });
     after(function cleanup () {
       delete PostsApp.prototype.props;
-      delete this.props;
-      delete this.propsSetCount;
+      delete this.propsArgs;
     });
   }
 };
@@ -41,36 +41,53 @@ const renderUtils = {
 // Start our tests
 describe.only('A request to GET /posts', function () {
   describe('with an empty set of posts', function () {
+    renderUtils.spyProps(PostsApp);
     httpUtils.save({
       url: serverUtils.getUrl('/posts'),
       expectedStatusCode: 200
     });
 
     it('renders PostsApp and outputs the same state', function () {
-      expect(this.$('body').text()).to.contain('No posts exist yet. Create one via "Create Post"');
-    });
-    it('provides browsers state with no posts', function () {
-      expect(this.$('script').html()).to.contain('window.__PRELOADED_STATE__ = {"posts":[]}');
+      // Verify props is non-empty
+      // DEV: We're unsure of where the additional post comes from
+      expect(this.propsArgs).to.have.lengthOf(2);
+
+      // Verify preloaded state is non-empty
+      const preloadedStateMatch = this.body.match(/window.__PRELOADED_STATE__ = ({[^<]+})/);
+      expect(preloadedStateMatch).to.not.equal(null);
+      const preloadedState = JSON.parse(preloadedStateMatch[1]);
+
+      // Compare our props and preloaded state to verify they're 1:1 and define their contract
+      expect(this.propsArgs[0]).to.deep.equal(preloadedState);
+      // TODO: Move contract into a conditional utility (same assert in CI mechanism as other contract)
+      fs.writeFileSync(__dirname + '/../test-files/render-contracts/posts-show-empty.json',
+        JSON.stringify(preloadedState, null, 2));
     });
   });
 
   describe('with multiple posts and comments', function () {
     testUtils.setFixtures([dbFixtures.POST_WITH_COMMENTS, dbFixtures.POST_ANOTHER]);
+    renderUtils.spyProps(PostsApp);
     httpUtils.save({
       url: serverUtils.getUrl('/posts'),
       expectedStatusCode: 200
     });
 
-    it('shows posts and comments', function () {
-      expect(this.$('body').text()).to.contain('This is an example post');
-      expect(this.$('body').text()).to.contain('This is an example comment');
-      expect(this.$('body').text()).to.contain('This is another example comment');
-      expect(this.$('body').text()).to.contain('This is another example post');
-    });
-    it('provides browsers state with posts and comments', function () {
-      expect(this.$('script').html()).to.contain('window.__PRELOADED_STATE__ = {"posts":[{');
-      expect(this.$('script').html()).to.contain('"content":"This is an example post"');
-      expect(this.$('script').html()).to.contain('"content":"This is an example comment"');
+    it('renders PostsApp and outputs the same state', function () {
+      // Verify props is non-empty
+      // DEV: We're unsure of where the additional post comes from
+      expect(this.propsArgs).to.have.lengthOf(2);
+
+      // Verify preloaded state is non-empty
+      const preloadedStateMatch = this.body.match(/window.__PRELOADED_STATE__ = ({[^<]+})/);
+      expect(preloadedStateMatch).to.not.equal(null);
+      const preloadedState = JSON.parse(preloadedStateMatch[1]);
+
+      // Compare our props and preloaded state to verify they're 1:1 and define their contract
+      expect(this.propsArgs[0]).to.deep.equal(preloadedState);
+      // TODO: Move contract into a conditional utility (same assert in CI mechanism as other contract)
+      fs.writeFileSync(__dirname + '/../test-files/render-contracts/posts-show-full.json',
+        JSON.stringify(preloadedState, null, 2));
     });
   });
 });
